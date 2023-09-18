880e22ea26f8213bfc9cd097b214943cb39380b4
Steppewolff <gomez.romano@gmail.com>
2023-09-18 22:27:58 +0200

    changes in portfolio and curriculum (CSS, JS y HTML)

--- static/js/scripts/portfolio.js
+++ static/js/scripts/portfolio.js
@@ -13,13 +13,20 @@ function initial(){
 
 function listen(){
     let tab = document.getElementsByClassName('nav-item');
-    var arrtab = Array.prototype.slice.call(tab) //Turns HTMLcollections type in array type, [].slice.call(tab) is also valid
+    var arrtab = Array.prototype.slice.call(tab); //Turns HTMLcollections type in array type, [].slice.call(tab) is also valid
+    console.log('arrtab: ', arrtab)
 
     arrtab.forEach(element => {
         element.addEventListener('click', function(){
-            if (document.getElementById('nav-inv-tab').classList.contains('show')){
-                document.getElementById('nav-inv-tab').classList.remove('show');
-            }
+            let show= document.getElementsByClassName('show');
+            let arrshow= Array.prototype.slice.call(show);
+            arrshow.forEach(show_tab => {
+                document.getElementById(show_tab.id).classList.remove('show');
+            })
+           
+            document.getElementById(element.id).classList.add('show');
+            document.getElementById(element.getAttribute('aria-controls')).classList.add('active');
+            document.getElementById(element.getAttribute('aria-controls')).classList.add('in');
         })
     });
 }
